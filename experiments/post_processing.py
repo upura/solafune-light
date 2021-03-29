774965a017a1cb2b61e212d577db0da5a55f2d47
upura <upura0@gmail.com>
2021-03-30 08:11:37 +0900

    Post processing 0.9

--- experiments/post_processing.py
+++ experiments/post_processing.py
@@ -3,5 +3,6 @@ import pandas as pd
 
 if __name__ == '__main__':
     sub = pd.read_csv('../output/submissions/submission_weight000.csv')
-    sub['LandPrice'] = sub.groupby('PlaceID')['LandPrice'].transform('mean').values
-    sub.to_csv('../output/submissions/submission_weight000_pp.csv', index=False)
+    sub['LandPrice'] = sub['LandPrice'] * 0.9
+    # sub['LandPrice'] = sub.groupby('PlaceID')['LandPrice'].transform('mean').values
+    sub.to_csv('../output/submissions/submission_weight000_08.csv', index=False)
